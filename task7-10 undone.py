f13570b529f5c4db251bdda4ac65cfa2b4d125ba
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-16 14:37:56 +0400

    commit 160324

--- task7-10 undone.py
+++ task7-10 undone.py
@@ -1,35 +1,22 @@
-from tkinter import *
+import tkinter as tk
+from mainWindow import MainWindow
 
+def show_popup():
+    popup = tk.Toplevel()
+    popup.geometry("250x350+200+100")
+    popup.title("Действие успешно выполнено")
+    label=tk.Label(popup,text="Действие успешно выполнено")
+    label.pack()
+    button = tk.Button(popup,command=popup.destroy)
+    button.pack()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("integerst")
 
-def circle():
-    c.create_oval(x, y, x + 30, y + 30)
+    file = "interest.ico"
+    root.iconbitmap(file)
 
+    app = MainWindow(root)
 
-def square():
-    c.create_rectangle(x, y, x + 30, y + 30)
-
-
-def triangle():
-    c.create_polygon(x, y, x - 15, y + 30, x + 15, y + 30,
-                     fill='white', outline='black')
-
-
-def popup(event):
-    global x, y
-    x = event.x
-    y = event.y
-    menu.post(event.x_root, event.y_root)
-
-
-x = 0
-y = 0
-
-root = Tk()
-c = Canvas(width=300, height=300, bg='white')
-c.pack()
-c.bind("<Button-3>", popup)
-menu = Menu(tearoff=0)
-menu.add_command(label="Круг", command=circle)
-menu.add_command(label="Квадрат", command=square)
-menu.add_command(label="Треугольник", command=triangle)
-root.mainloop()
+    root.bind("<Escape>", app.quit)
+    root.mainloop()
